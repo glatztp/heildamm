ad0f32afe910dd4fbd695ee877f06380350ea4cf
gabriel glatz <gabrielfellipeglatz@gmail.com>
2026-04-08 18:57:44 -0300

    first commit

--- .vscode-time-tracker/src/crypto/index.ts
+++
@@ -1,8 +0,0 @@
-export { KeyManager } from "./key-manager";
-export { EncryptionService } from "./encryption";
-export { CompressionService } from "./compression-service";
-export {
-  ConsolidationService,
-  ConsolidatedStats,
-  ConsolidationStats,
-} from "./consolidation-service";
